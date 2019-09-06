613711fc762dbfca88fc3b07f5cee01e19181d48
Christopher-Nall <bigcmn@gmail.com>
2019-09-06 15:47:28 -0500

    I have done the project and only have worked on it for a day because I was gone the day before

---
+++ Functional Turtle gallery.py
@@ -0,0 +1,80 @@
+
+
+
+import turtle
+
+
+size = 0
+
+turtle.speed(10000)
+turtle.penup()
+
+turtle.goto(-200, 200)
+turtle.pendown()
+turtle.color("orange")
+
+def star(turtle, side):
+    for i in range(5):
+        turtle.forward(size)
+        turtle.right(144)
+
+star(turtle, 75)
+
+for i in range(50):
+    star(turtle, 75)
+    turtle.right(5)
+    size += 3
+
+turtle.color("blue")
+turtle.penup()
+turtle.goto(200, 200)
+turtle.pendown()
+
+def square(turtle,side):
+    for i in range(4):
+        turtle.right(90)
+        turtle.forward(100)
+
+
+
+for i in range(60):
+    square(turtle,75)
+    turtle.left(5)
+
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+size = 0
+
+
+def square(turtle, side):
+    for i in range(5):
+        turtle.forward(size)
+        turtle.right(90)
+
+
+square(turtle, 75)
+
+for i in range(50):
+    square(turtle, 75)
+    turtle.right(5)
+    size += 3
+
+turtle.penup()
+turtle.color("firebrick")
+turtle.goto(200, -200)
+turtle.pendown()
+
+
+def star(turtle,side):
+    for i in range(4):
+        turtle.right(144)
+        turtle.forward(100)
+
+
+for i in range(60):
+    square(turtle,75)
+    turtle.left(5)
+
+
+turtle.exitonclick()
